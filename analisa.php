f80b126ba15846375b45ded0e8a531fff7d4d12e
Levi <levibrito020@gmail.com>
2024-12-21 14:30:12 -0300

    versão 3

--- analisa.php
+++ analisa.php
@@ -1,62 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
 
-session_start();
-$hostname = "localhost";
-$user= "root";
-$pass = "";
-$db = "escola";
-
-$conn = new mysqli($hostname, $user, $pass, $db);
+include "conn.php";
 
 if($conn->connect_error){
     echo "server morreu".$conn->connect_errno;
 }else{
-    $turma = $conn->real_escape_string($_POST["turma"]);
-    $idAluno = $conn->real_escape_string($_POST["idAluno"]);
-    $tri = $conn->real_escape_string($_POST["tri"]);
-
-    if(!empty($tri) && !empty($idAluno) && !empty($turma)){
-        $p1 = doubleval($_POST["p1"]);
-        $p2 = doubleval($_POST["p2"]);
-        $p3 = doubleval($_POST["p3"]);
-
-        $m1 = doubleval($_POST["m1"]);
-        $m2 = doubleval($_POST["m2"]);
-        $m3 = doubleval($_POST["m3"]);
-
-        $ch1 = doubleval($_POST["ch1"]);
-        $ch2 = doubleval($_POST["ch2"]);
-        $ch3 = doubleval($_POST["ch3"]);
-
-        $ce1 = doubleval($_POST["ce1"]);
-        $ce2 = doubleval($_POST["ce2"]);
-        $ce3 = doubleval($_POST["ce3"]);
-
-        $ef1 = doubleval($_POST["ef1"]);
-        $ef2 = doubleval($_POST["ef2"]);
-        $ef3 = doubleval($_POST["ef3"]);
-
-        if(!empty($p1) && !empty($m1) && !empty($ch1) && !empty($ce1) && !empty($ef1)){
-            if(!empty($p2) && !empty($m2) && !empty($ch2) && !empty($ce2) && !empty($ef2)){
-                if(!empty($p3) && !empty($m3) && !empty($ch3) && !empty($ce3) && !empty($ef3)){
-                    echo "todos os campos foram preenchidos";
+    if (!empty($_POST["tri"]) && !empty($_POST["turma"]) && !empty($_POST["idAluno"])){
+        //analise de dados
+        if(!empty($_POST["p1"]) && !empty($_POST["m1"]) && !empty($_POST["ch1"]) && !empty($_POST["ce1"]) && !empty($_POST["ef1"])){
+            if(!empty($_POST["p2"]) && !empty($_POST["m2"]) && !empty($_POST["ch2"]) && !empty($_POST["ce2"]) && !empty($_POST["ef2"])){
+                if(!empty($_POST["p3"]) && !empty($_POST["m3"]) && !empty($_POST["ch3"]) && !empty($_POST["ce3"]) && !empty($_POST["ef3"])){
+                    //todos os campos
                 }else{
-                    
+                    //apenas 1 e 2
+                }
+            }else{
+                if(!empty($_POST["p3"]) && !empty($_POST["m3"]) && !empty($_POST["ch3"]) && !empty($_POST["ce3"]) && !empty($_POST["ef3"])){
+                    //1 e 3
+                }else{
+                    //apenas campo 1
                 }
             }
-        }elseif(!empty($p2) && !empty($m2) && !empty($ch2) && !empty($ce2) && !empty($ef2)){
-
-        }elseif(!empty($p3) && !empty($m3) && !empty($ch3) && !empty($ce3) && !empty($ef3)){
-
+        }elseif(!empty($_POST["p2"]) && !empty($_POST["m2"]) && !empty($_POST["ch2"]) && !empty($_POST["ce2"]) && !empty($_POST["ef2"])){
+            if(!empty($_POST["p3"]) && !empty($_POST["m3"]) && !empty($_POST["ch3"]) && !empty($_POST["ce3"]) && !empty($_POST["ef3"])){
+                //campo 2 e 3
+            }else{
+                //campo 2 apenas
+            }   
+        }elseif(!empty($_POST["p3"]) && !empty($_POST["m3"]) && !empty($_POST["ch3"]) && !empty($_POST["ce3"]) && !empty($_POST["ef3"])){
+            //apenas campo 3";
         }else{
             echo "<script>
-                alert('nenhum campo foi inserido');
-                window.location.href = 'pagprincipal.php';
+            alert('nenhum campo preenchido');
+            window.location.href = 'pagprincipal.php';
             </script>";
         }
     }else{
         echo "<script>
-            alert('Falha, dados do servidor');
+            alert('erro ao se comunicar com dados essenciais do servidor');
             window.location.href = 'pagprincipal.php';
         </script>";
     }
